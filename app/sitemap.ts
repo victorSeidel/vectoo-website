430a86e2537920c324e6b06450d64a32fd8006a8
Victor Seidel <victorgabrielms88@gmail.com>
2026-07-13 22:53:06 -0300

    REFACTOR: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,27 +7,11 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap>
     const baseUrl = 'https://vectoo.com.br';
 
     const posts = await findAllPublicPosts();
-
-    const postsRoutes = posts.map((post) => ({
-        url: `${baseUrl}/blog/post/${post.slug}`,
-        lastModified: new Date(post.updatedAt),
-        changeFrequency: 'weekly' as const,
-        priority: 0.5,
-    }));
+    const postsRoutes = posts.map((post) => ({ url: `${baseUrl}/blog/post/${post.slug}` }));
 
     return [
-        {
-            url: baseUrl,
-            lastModified: new Date(),
-            changeFrequency: 'yearly' as const,
-            priority: 1,
-        },
-        {
-            url: `${baseUrl}/blog`,
-            lastModified: new Date(),
-            changeFrequency: 'yearly' as const,
-            priority: 0.7,
-        },
+        { url: baseUrl },
+        { url: `${baseUrl}/blog` },
         ...postsRoutes,
     ]
 }
